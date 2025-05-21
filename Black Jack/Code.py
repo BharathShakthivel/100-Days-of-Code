b77a46c7a0f768e5c4913c636a7161c6ac487db8
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2025-05-21 23:12:18 +0100

    Update Code.py

--- Black Jack/Code.py
+++ Black Jack/Code.py
@@ -1,1 +1,10 @@
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+option = input("Do you want to play the game 'y' or 'n'? ")
+
+if option.lower() == "y":
+ game = True
+ while game:
+  import art
+  print(art.logo)
+  

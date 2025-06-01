273acaf7aeac920f3669ea358de2c008f9443339
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2025-06-01 22:42:52 +0100

    Update Code.py

--- Number Guessing Game/Code.py
+++ Number Guessing Game/Code.py
@@ -2,6 +2,13 @@ import art
 print(art.logo)
 
 print("Welcome to Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100")
+print("I'm thinking of a number between 1 and 100.")
 
 difficulty = input("Choose a difficulty. Typer 'easy' or 'hard': ")
+life = 0
+if difficulty.lower() == "easy":
+    life = 10
+else:
+    life = 5
+print(f"You have {life} attempts remaining to guess the number")
+guess = input("Make a Guess: ")

b18358c6c0f0c2d14a8974a5ff3ef217c6a36ef1
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2025-09-17 10:58:52 +0100

    Create main.py

---
+++ Etch-A-Sketch/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+
+# <--------------- Etch a sketch--------------->
+
+def move_forwards():
+    tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+def move_counter_clockwise():
+    tim.left(10)
+def move_clockwise():
+    tim.right(10)
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
+
+
+
